bb06108b9991ac7411b11ae1be363936f1a74809
mariap <maria.paktiti@gmail.com>
2016-01-31 11:03:50 +0200

    Initial commit

---
+++ GithubOAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubOAuth.Startup))]
+namespace GithubOAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
